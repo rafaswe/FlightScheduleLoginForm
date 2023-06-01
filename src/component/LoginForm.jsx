606e0bc77e33be41c6fe95622c109a1b7940094c
mahiyarahmanrafa <mahiyarahmanrafa@usbd.com.bd>
2023-06-01 17:53:12 +0600

    Responsiveness solve

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -9,10 +9,10 @@ const LoginForm = () => {
     <div className='mt-lg-5 pt-lg-5 '>
       <div className='customWidth mx-auto '>
         <div className="row gx-5 gy-2 mt-2">
-          <div className="col-lg-7 col-sm-12 ">
+          <div className="col-md-7 col-sm-12 ">
             <img src={planPhoto} className='w-100 h-100 customRadius customDisplay' alt="" />
           </div>
-        <div className="col-lg-5 col-sm-12 customRadius bg-white p-5 shadow">
+        <div className="col-md-5 col-sm-12 customRadius bg-white p-5 shadow">
             <img src={flightLogo} alt="" className='d-block mx-auto mb-4  '  />
             <p className='fs-2 customMargin'>Login to account</p>
             <MainForm />
